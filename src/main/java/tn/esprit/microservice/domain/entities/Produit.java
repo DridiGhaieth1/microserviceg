be9ed352cb7f1495fc84e34be3c9e62b7d3fe001
Ghaieth Dridi <ghaieth.dridi@esprit.tn>
2023-05-08 11:05:57 +0100

    create entites

---
+++ src/main/java/tn/esprit/microservice/domain/entities/Produit.java
@@ -0,0 +1,59 @@
+package tn.esprit.microservice.domain.entities;
+
+
+import jakarta.persistence.*;
+
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "produit")
+public class Produit implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nom")
+    private String nom;
+
+    @Column(name = "description")
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "categorie_id")
+    private Categorie categorie;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Categorie getCategorie() {
+        return categorie;
+    }
+
+    public void setCategorie(Categorie categorie) {
+        this.categorie = categorie;
+    }
+}
+
